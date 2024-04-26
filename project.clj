478ab7d00ae9d22bce3be888ef4f0f6848841639
Brandon Vincent <brandon@amperity.com>
2024-04-26 12:09:13 -0700

    Make lein repl work

--- project.clj
+++ project.clj
@@ -15,7 +15,11 @@
   {"uberjar" ["with-profile" "v1:v2" "uberjar"]}
 
   :profiles
-  {:v1
+  {;; Use :v1 by default, for example for `lein repl`.
+   :default
+   [:base :system :user :provided :v1 :dev]
+
+   :v1
    {:uberjar-name "demo_3.5.1.jar"
     :dependencies [[org.apache.spark/spark-core_2.12 "3.5.1"]]}
 
