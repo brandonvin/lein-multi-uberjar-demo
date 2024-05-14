43146348a2152774f33b5ab7465a28e6b65ecef3
Brandon Vincent <brandon@amperity.com>
2024-05-14 10:41:30 -0700

    Fix early exit in script

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-lein test
-lein do clean, uberjar
+lein do clean, test, uberjar
 echo
 
 for JAR in $(find target/uberjar -maxdepth 1 -name '*.jar' -not -name '*SNAPSHOT*'); do
-    jar -tf $JAR | grep SparkSession
+    echo "Found uberjar $JAR, checking for unintentionally bundled Spark classes:"
+    if jar -tf $JAR | grep SparkSession; then
+        echo "Fail"
+    else
+        echo "Pass"
+    fi
 done
